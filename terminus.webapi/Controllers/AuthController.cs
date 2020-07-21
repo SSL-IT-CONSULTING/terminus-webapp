a9697c3339941abcf8c463069291cb93e25a7493
paullucban <36031571+paullucban@users.noreply.github.com>
2020-07-21 22:35:01 +0800

    update backend
    update backend

---
+++ terminus.webapi/Controllers/AuthController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using terminus.shared.models;
+
+namespace terminus.webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<AppUser> _signInMgr;
+        private readonly UserManager<AppUser> _userMgr;
+        private readonly RoleManager<AppRole> _roleMgr;
+        private readonly IPasswordHasher<AppUser> _hasher;
+        private readonly IConfiguration _config;
+
+        public AuthController(SignInManager<AppUser> signInMgr,
+            UserManager<AppUser> userMgr,
+             RoleManager<AppRole> roleMgr,
+             IPasswordHasher<AppUser> hasher,
+             IConfiguration config
+            )
+        {
+            _signInMgr = signInMgr;
+            _userMgr = userMgr;
+            _roleMgr = roleMgr;
+            _hasher = hasher;
+            _config = config;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> AuthenticateAsync([FromBody] LoginViewModel model)
+        {
+            var user = await _userMgr.FindByNameAsync(model.userName);
+            if (user != null)
+            {
+                if (_hasher.VerifyHashedPassword(user, user.PasswordHash, model.password) ==
+                     PasswordVerificationResult.Success)
+                {
+                    var userClaims = await _userMgr.GetClaimsAsync(user);
+                    var claims = new[]
+                    {
+                                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+                            }.Union(userClaims);
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        issuer: _config["Jwt:issuer"],
+                        audience: _config["Jwt:issuedTo"],
+                        claims: claims,
+                        expires: DateTime.UtcNow.AddMinutes(20),
+                        signingCredentials: creds
+                    ); ;
+
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+                    return Ok(new 
+                    { 
+                        token = tokenString,
+                        expiration = token.ValidTo
+                    });
+                }
+            }
+
+            return Unauthorized();
+
+
+        }
+    }
+}
---
+++ terminus.webapi/Startup.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using terminus.dataaccess;
+using terminus.shared.models;
+
+namespace terminus.webapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration["connectionStrings:dbconn"];
+
+            services.AddDbContext<AppDBContext>(options =>
+                   options.UseSqlServer(
+                      connectionString));
+
+            services.AddIdentity<AppUser, AppRole>(options => {
+                options.SignIn.RequireConfirmedEmail = false;
+                options.User.AllowedUserNameCharacters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+\";
+
+            })
+                .AddEntityFrameworkStores<AppDBContext>();
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            //SQL database Connnection(name defined in appsettings.json)
+            
+            var SQLConnectionConfiguration = new SQLConnectionConfiguration(connectionString);
+            services.AddSingleton(SQLConnectionConfiguration);
+
+            
+            services.AddScoped<DapperManager, DapperManager>();
+            services.AddTransient<IdentitySeeder>();
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:issuer"],
+                    ValidAudience = Configuration["Jwt:issuedTo"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+
+            ConfigureSwagger(services);
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
